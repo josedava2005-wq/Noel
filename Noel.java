f3025ec8938b74f431978acdb4c99a8e0cd7a4c7
David0073 <velenciamartinezjosedavid@gmail.com>
2026-09-07 23:36:34 -0500

    feat: proceso de iventario

---
+++ Noel.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Noel{
+
+    //me ayuda buscando un producto
+    public static boolean buscarProducto(String[] verduras, String nombreBuscado) {
+        for (string verdura : verduras)
+}
